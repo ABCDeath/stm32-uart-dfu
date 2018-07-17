0de471c52564f9493d5a66a0a7919dca59a30b8b
ABCDeath <cob19721@yandex.ru>
2018-07-17 23:15:48 +0300

    Stm32 uart dfu class prototype.

--- stm32_uart_dfu.py
+++ stm32_uart_dfu.py
@@ -1,82 +1,96 @@
 import serial
 
 
-__DFU_RESPONSE_SIZE = 1
+class Stm32UartDfu:
+    __DEFAULT_PARAMETERS = {
+        'baudrate': 115200,
+        'parity': 'E',
+        'timeout': 1  # seconds
+    }
 
-__DFU_RESPONSE = {
-    'ack': 0x79.to_bytes(length=1, byteorder='little'),
-    'nack': 0x7f.to_bytes(length=1, byteorder='little')
-}
+    __RESPONSE_SIZE = 1
 
-__DFU_COMMAND = {
-    'get version': 0x0,
-    'get version and protection status': 0x01,
-    'get id': 0x02,
-    'read memory': 0x11,
-    'go': 0x21,
-    'write memory': 0x31,
-    'erase': 0x43,
-    'extended erase': 0x44,
-    'write protect': 0x63,
-    'write unprotect': 0x73,
-    'readout protect': 0x82,
-    'readout unprotect': 0x92
-}
+    __RESPONSE = {
+        'ack': 0x79.to_bytes(length=1, byteorder='little'),
+        'nack': 0x7f.to_bytes(length=1, byteorder='little')
+    }
 
-def uart_dfu_init(serial_port):
-    __DFU_INIT_SEQUENCE = [0x7f]
+    __COMMAND = {
+        'get version': 0x0,
+        'get version and protection status': 0x01,
+        'get id': 0x02,
+        'read memory': 0x11,
+        'go': 0x21,
+        'write memory': 0x31,
+        'erase': 0x43,
+        'extended erase': 0x44,
+        'write protect': 0x63,
+        'write unprotect': 0x73,
+        'readout protect': 0x82,
+        'readout unprotect': 0x92
+    }
 
-    bytes_sent = serial_port.write(__DFU_INIT_SEQUENCE)
-    if bytes_sent != len(__DFU_INIT_SEQUENCE):
-        # TODO: use some exceptions
-        pass
+    def _receive_response(self):
+        response = self._port_handle.read(1)
+        if len(response) == 0:
+            print('read timeout')
+        else:
+            if response != self.__RESPONSE['ack']:
+                print('dfu answered nack')
+        return response
 
-    receive_response(serial_port)
+    def _uart_dfu_init(self):
+        __INIT_SEQUENCE = [0x7f]
 
+        bytes_sent = self._port_handle.write(__INIT_SEQUENCE)
+        if bytes_sent != len(__INIT_SEQUENCE):
+            # TODO: use some exceptions
+            pass
 
-def receive_response(serial_port):
-    response = serial_port.read(1)
-    if len(response) == 0:
-        print('read timeout')
-    else:
-        if response != __DFU_RESPONSE['ack']:
-            print('dfu answered nack')
-    return response
+        self._receive_response()
 
+    def __init__(self, port):
+        self._port_handle = serial.Serial(port=port,
+                                          baudrate=self.__DEFAULT_PARAMETERS[
+                                              'baudrate'],
+                                          parity=self.__DEFAULT_PARAMETERS[
+                                              'parity'],
+                                          timeout=self.__DEFAULT_PARAMETERS[
+                                              'timeout'])
+        if not self._port_handle.isOpen():
+            print('Can not open serial port.')
 
-def send_command(serial_port, command):
-    checksum = 0xff - command
-    command_sequence = [command, checksum]
+        self._uart_dfu_init()
 
-    bytes_sent = serial_port.write(command_sequence)
-    if bytes_sent != len(command_sequence):
-        # TODO: use some exceptions
-        pass
+    def __delete__(self):
+        if self._port_handle.isOpen():
+            self._port_handle.close()
 
-    receive_response(serial_port)
+    def _send_command(self, command):
+        checksum = 0xff - command
+        command_sequence = [command, checksum]
 
+        bytes_sent = self._port_handle.write(command_sequence)
+        if bytes_sent != len(command_sequence):
+            # TODO: use some exceptions
+            pass
 
-def get_id(serial_port):
-    send_command(serial_port, __DFU_COMMAND['get id'])
+        self._receive_response()
 
-    size = int.from_bytes(serial_port.read(1), 'little') + 1
-    pid = serial_port.read(size)
+    def get_id(self):
+        self._send_command(self.__COMMAND['get id'])
 
-    receive_response(serial_port)
+        size = int.from_bytes(self._port_handle.read(1), 'little') + 1
+        pid = self._port_handle.read(size)
 
-    return pid
+        self._receive_response()
+
+        return pid
 
 
 if __name__ == '__main__':
-    serial_port = serial.Serial(port='/dev/ttyUSB0', baudrate=115200,
-                                parity='E', timeout=1)
-    if not serial_port.is_open:
-        print('Can not open serial port.')
+    dfu = Stm32UartDfu('/dev/ttyUSB0')
 
-    uart_dfu_init(serial_port)
-
-    pid = get_id(serial_port)
+    pid = dfu.get_id()
 
     print('mcu id: 0x{}'.format(pid.hex()))
-
-    serial_port.close()
