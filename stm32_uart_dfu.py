3bcca0a4ec41331184c995167d0e55e4383438ee
ABCDeath <cob19721@yandex.ru>
2018-07-17 22:21:16 +0300

    Added get id function, added command dict.

--- stm32_uart_dfu.py
+++ stm32_uart_dfu.py
@@ -1,9 +1,26 @@
 import serial
 
 
+__DFU_RESPONSE_SIZE = 1
+
 __DFU_RESPONSE = {
     'ack': 0x79.to_bytes(length=1, byteorder='little'),
     'nack': 0x7f.to_bytes(length=1, byteorder='little')
+}
+
+__DFU_COMMAND = {
+    'get version': 0x0,
+    'get version and protection status': 0x01,
+    'get id': 0x02,
+    'read memory': 0x11,
+    'go': 0x21,
+    'write memory': 0x31,
+    'erase': 0x43,
+    'extended erase': 0x44,
+    'write protect': 0x63,
+    'write unprotect': 0x73,
+    'readout protect': 0x82,
+    'readout unprotect': 0x92
 }
 
 def uart_dfu_init(serial_port):
@@ -14,12 +31,17 @@ def uart_dfu_init(serial_port):
         # TODO: use some exceptions
         pass
 
+    receive_response(serial_port)
+
+
+def receive_response(serial_port):
     response = serial_port.read(1)
     if len(response) == 0:
         print('read timeout')
     else:
         if response != __DFU_RESPONSE['ack']:
             print('dfu answered nack')
+    return response
 
 
 def send_command(serial_port, command):
@@ -31,12 +53,18 @@ def send_command(serial_port, command):
         # TODO: use some exceptions
         pass
 
-    response = serial_port.read(1)
-    if len(response) == 0:
-        print('read timeout')
-    else:
-        if response != __DFU_RESPONSE['ack']:
-            print('dfu answered nack')
+    receive_response(serial_port)
+
+
+def get_id(serial_port):
+    send_command(serial_port, __DFU_COMMAND['get id'])
+
+    size = int.from_bytes(serial_port.read(1), 'little') + 1
+    pid = serial_port.read(size)
+
+    receive_response(serial_port)
+
+    return pid
 
 
 if __name__ == '__main__':
@@ -47,12 +75,8 @@ if __name__ == '__main__':
 
     uart_dfu_init(serial_port)
 
-    send_command(serial_port, 0x2)
+    pid = get_id(serial_port)
 
-    ret = serial_port.read(4)
-    if len(ret) == 0:
-        print('read timeout')
-    else:
-        print('read: {}'.format(ret))
+    print('mcu id: 0x{}'.format(pid.hex()))
 
     serial_port.close()
