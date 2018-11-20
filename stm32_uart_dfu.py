4cf10222a311396fc3be99052719a836c7f8dc7b
ABCDeath <cob19721@yandex.ru>
2018-11-20 12:19:58 +0300

    Fix in read method.

--- stm32_uart_dfu.py
+++ stm32_uart_dfu.py
@@ -40,7 +40,7 @@ def _retry(retry_num=0, action='', exc_call=None):
                     ret = func(*args, **kwargs)
                 except DfuException as ex:
                     if exc_call:
-                        exc_call()
+                        exc_call(*args)
                     last_caught = ex
                 else:
                     return ret
@@ -269,6 +269,16 @@ class Stm32UartDfu:
 
         return version, read_protection_status
 
+    @_retry(_RETRIES, 'go', _serial_flush)
+    def go(self, address):
+        """
+        Runs MCU from memory defined by address parameter.
+        :param address: int - address to jump
+        """
+
+        self._send_command(self._COMMAND['go'])
+        self._set_address(address)
+
     def read(self, address, size, progress_update=None):
         """
         Reads %size% bytes of memory from %address%.
@@ -289,7 +299,7 @@ class Stm32UartDfu:
             part_size = min(self._RW_MAX_SIZE, size_remain)
             offset = address + size - size_remain
 
-            data = b''.join([data, self._read_memory_chunk(offset, size)])
+            data = b''.join([data, self._read_memory_chunk(offset, part_size)])
 
             size_remain -= part_size
 
@@ -297,16 +307,6 @@ class Stm32UartDfu:
             progress_update(100)
 
         return data
-
-    @_retry(_RETRIES, 'go', _serial_flush)
-    def go(self, address):
-        """
-        Runs MCU from memory defined by address parameter.
-        :param address: int - address to jump
-        """
-
-        self._send_command(self._COMMAND['go'])
-        self._set_address(address)
 
     def write(self, address, data, progress_update=None):
         """
@@ -472,7 +472,7 @@ class DfuCommandHandler:
             bar_thread.update(-1)
             bar_thread.join()
 
-        print(f'Error: {ex}', self._EXCEPTION_MESSAGE, sep='\n')
+        print(f'Error: {ex}', EXCEPTION_MESSAGE, sep='\n')
 
     def get_id(self, dfu, args):
         print('MCU ID: 0x{}'.format(dfu.get_id().hex()))
@@ -497,7 +497,8 @@ class DfuCommandHandler:
         bar_thread = ProgressBarThread(endless=True)
 
         try:
-            dfu.erase(args.address, args.size, mem_map, bar_thread.update)
+            dfu.erase(int(args.address, 0), int(args.size, 0),
+                      mem_map, bar_thread.update)
         except DfuException as ex:
             self._abort(ex, bar_thread)
             return
@@ -534,7 +535,8 @@ class DfuCommandHandler:
             bar_thread = ProgressBarThread(endless=True)
 
             try:
-                dfu.erase(args.address, erase_size, mem_map, bar_thread.update)
+                dfu.erase(int(args.address, 0), erase_size, mem_map,
+                          bar_thread.update)
             except DfuException as ex:
                 self._abort(ex, bar_thread)
                 return
@@ -585,57 +587,78 @@ if __name__ == '__main__':
     _ARGS_HELP = {
         'address': 'Memory address for ',
         'size': 'Required size of memory to be ',
-        'memmap': 'Json file, containing memory structure. Format: [{"address": "value", "size": "value"}, ...]',
+        'memmap': 'Json file, containing memory structure.'
+                  'Format: [{"address": "value", "size": "value"}, ...]',
         'run': 'Run program after loading.',
-        'erase': 'Erase memory enough to store firmware (whole memory if no memory map).'
+        'erase': 'Erase memory enough to store firmware'
+                 '(whole memory if no memory map).'
     }
 
     dfu_handler = DfuCommandHandler()
 
     arg_parser = argparse.ArgumentParser(description='Stm32 uart dfu utility.')
-    arg_parser.add_argument('-p', '--port', default='/dev/ttyUSB0',
-                            help='Serial port file (for example: /dev/ttyUSB0).')
+
+    arg_parser.add_argument(
+        '-p', '--port', default='/dev/ttyUSB0',
+        help='Serial port file (for example: /dev/ttyUSB0).')
 
     commands = arg_parser.add_subparsers()
 
     load_command = commands.add_parser('load')
-    load_command.add_argument('-a', '--address', default='0x8000000',
-                              help=' '.join([_ARGS_HELP['address'],
-                                             'loading binary file.']))
+
+    load_command.add_argument(
+        '-a', '--address', default='0x8000000',
+        help=' '.join([_ARGS_HELP['address'], 'loading binary file.']))
+
     load_command.add_argument('-e', '--erase', action='store_true',
                               help=_ARGS_HELP['erase'])
+
     load_command.add_argument('-f', '--file', help='Binary firmware file.')
+
     load_command.add_argument('-m', '--memory-map', default=None,
                               help=_ARGS_HELP['memmap'])
+
     load_command.add_argument('-r', '--run', action='store_true',
                               help=_ARGS_HELP['run'])
+
     load_command.set_defaults(func=dfu_handler.load)
 
     erase_command = commands.add_parser('erase')
-    erase_command.add_argument('-a', '--address', default='0x8000000',
-                               help=' '.join(
-                                   [_ARGS_HELP['address'], 'erasing.']))
+
+    erase_command.add_argument(
+        '-a', '--address', default='0x8000000',
+        help=' '.join([_ARGS_HELP['address'], 'erasing.']))
+
     erase_command.add_argument('-m', '--memory-map', default=None,
                                help=_ARGS_HELP['memmap'])
+
     erase_command.add_argument('-s', '--size', default=None,
                                help=' '.join([_ARGS_HELP['size'], 'erased.']))
+
     erase_command.set_defaults(func=dfu_handler.erase)
 
     dump_command = commands.add_parser('dump')
+
     dump_command.add_argument('-a', '--address', default='0x8000000',
                               help=' '.join([_ARGS_HELP['address'], 'dump.']))
+
     dump_command.add_argument('-s', '--size', default=None,
                               help=' '.join([_ARGS_HELP['size'], 'dumped.']))
+
     dump_command.add_argument('-f', '--file',
                               help='Specify file for memory dump.')
+
     dump_command.set_defaults(func=dfu_handler.dump)
 
     get_id_command = commands.add_parser('id')
+
     get_id_command.set_defaults(func=dfu_handler.get_id)
 
     run_command = commands.add_parser('run')
+
     run_command.add_argument('-a', '--address', default='0x8000000',
                              help=' '.join([_ARGS_HELP['address'], 'run.']))
+
     run_command.set_defaults(func=dfu_handler.run)
 
     args = arg_parser.parse_args()
