b941d1f8606e4c60fcf74725f79f4f4cd66c4772
ABCDeath <cob19721@yandex.ru>
2019-11-19 14:33:28 +0300

    Move tests to it's directory

--- tests/test_dfu.py
+++ tests/test_dfu.py
@@ -1,12 +1,14 @@
 import json
-import pytest
 import random
+
+import pytest
 
 
 @pytest.fixture(scope='module', params=['/dev/ttyUSB0'])
 def dfu(request):
     import stm32uartdfu
     return stm32uartdfu.Stm32UartDfu(request.param)
+
 
 @pytest.fixture(scope='module', params=['memory_map/stm32f407.json'])
 def memory_map(request):
@@ -19,17 +21,22 @@ def firmware(request):
     with open(request.param, 'rb') as fw:
         return fw.read()
 
+
 def test_read_id(dfu):
     assert len(dfu.id) == 2 and dfu.id[0] == 4
+
 
 def test_version(dfu):
     assert dfu.version
 
+
 def test_read_protection_status(dfu):
     assert dfu.read_protection_status
 
+
 def test_commands(dfu):
     assert b'\x00\x01\x02\x11\x21\x31\x44' in dfu.commands
+
 
 @pytest.mark.parametrize("address,size", [
     (0x8000040, 76543),
